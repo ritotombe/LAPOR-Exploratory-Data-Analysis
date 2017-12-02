355c1913f75a7e5d482fcb79c5fe2e5de407bb5b
ritotombe <ritotombe@gmail.com>
2017-12-02 15:01:08 +1100

    wrangle

---
+++ wrangle.R
@@ -0,0 +1,34 @@
+library(ggplot2)
+
+report <- read.csv(file = 'data/data.csv')
+area <- read.csv(file = 'data/daftararea.csv', sep = ';', quote = '"')
+
+head(area)
+head(report)
+
+report$id <- factor(report$id) 
+area$Area <- area$area_name # comply column name for area name on both dataframes to be merge on that (I want to get the Lat and Lon)
+
+total <- merge(area,report,by="Area")
+
+total <- na.omit(total)
+
+#remove unnecessary columns
+total <- total[, !names(total) %in% c("parent_id", "area_name", "X", "TrackingID", "KategoriID", "DisposisiInstansiID", "AreaID", "TanggalDisposisi", "TanggalLaporanMasuk", "TanggalLaporanAktivitasTerakhir", "TanggalLaporanDitutup", "TimestampTanggalDisposisi")]
+
+#rename column's names
+#https://stackoverflow.com/questions/6081439/changing-column-names-of-a-data-frame
+
+colnames(total) <- c("area", 
+                     "latitude", 
+                     "longitude", 
+                     "id", 
+                     "reporter",
+                     "category", 
+                     "related_department", 
+                     "status",
+                     "report_issued",
+                     "report_last_activity",
+                     "report_closed",
+                     "created_at",
+                     "updated_at")
